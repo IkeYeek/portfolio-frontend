9dca7cfe5047b5c85459a3d970d57e556b43c056
Lucas Ike <lucas.marques@u-bordeaux.fr>
2023-07-11 15:20:49 +0200

    added construction logo

---
+++ src/components/WorkingOnIt.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="img-root">
+    <img alt="Site En Construction" src="../assets/img/construction.png" />
+  </div>
+</template>
+
+<style scoped>
+#img-root {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
